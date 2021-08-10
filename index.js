5edaf96e8dcb3cdcef7caab1972d540f0a82a1bb
Jesse Coad <jessecoad@gmail.com>
2021-08-11 01:05:50 +1000

    heroku

--- index.js
+++ index.js
@@ -21,15 +21,19 @@ async function startApolloServer() {
   // app.get("*", (req, res) => {
   //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   // });
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+  // app.use(express.urlencoded({ extended: true }));
+  // app.use(express.json());
 
-  if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/build")));
-  }
+  // if (process.env.NODE_ENV === "production") {
+  //   app.use(express.static(path.join(__dirname, "/client/build")));
+  // }
 
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  // });
+  app.use(express.static(path.join(__dirname, "client", "build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 
   await new Promise((resolve) =>
