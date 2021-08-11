c6926c3449618dec38e4e97ed74de7bcf1d3bf83
Jesse Coad <jessecoad@gmail.com>
2021-08-11 11:12:39 +1000

    restore db connect

--- databases/database-connection.js
+++ databases/database-connection.js
@@ -1,22 +1,22 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  console.log("connected with mongodb");
-});
-
 // const mongoose = require("mongoose");
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Imbibedb", {
+// mongoose.connect(process.env.MONGODB_URI, {
 //   useNewUrlParser: true,
+//   // useCreateIndex: true,
+//   // useFindAndModify: false,
 //   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
 // });
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function () {
+//   console.log("connected with mongodb");
+// });
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Imbibedb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
